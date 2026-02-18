7b3b7b90ccf27c2536269512349701438579ccfc
sunitgopal <139268227+sunitgopal@users.noreply.github.com>
2026-02-18 19:42:07 +0000

    refactor: Replace array-based scheduler with TCB linked list

--- kernel.s
+++ kernel.s
@@ -1,24 +1,42 @@
 /* kernel.s - The OS Data Structures */
 
-.syntax unified     /* Use modern Thumb-2 syntax */
+
+.syntax unified
 .cpu cortex-m3
 .thumb
 
-
 .section .data
-    .p2align 2
-
-/* ---------
- KERNEL VARIABLES ((The "State" of the OS))
-   ---------
-*/
-
-
-.global current_task
-current_task:       /* The idx of the currently running task (0-3) */
-    .word 0         /* Start with Task 0 */
-
 .p2align 2
-.global task_sps
-task_sps:           /* Array of initial-SPs for each Task */
-    .word stack0, stack1, stack2, stack3
+
+/* -------------------------------------------------------------------------
+   GLOBAL KERNEL POINTER
+   ------------------------------------------------------------------------- */
+.global current_tcb
+current_tcb:
+    .word tcb0          // Pointer to the TCB of the running task
+
+/* -------------------------------------------------------------------------
+   TASK CONTROL BLOCKS (Linked List)
+   Structure:
+     Offset 0: SP
+     Offset 4: Next TCB
+   ------------------------------------------------------------------------- */
+.global tcb0
+tcb0:
+    .word stack0        // Initial SP
+    .word tcb1          // Points to Task 1
+
+.global tcb1
+tcb1:
+    .word stack1
+    .word tcb2          // Points to Task 2
+
+.global tcb2
+tcb2:
+    .word stack2
+    .word tcb3          // Points to Task 3
+
+.global tcb3
+tcb3:
+    .word stack3
+    .word tcb0          // Points back to Task 0 (Circular!)
--- systick_handler.s
+++ systick_handler.s
@@ -1,11 +1,12 @@
 .syntax unified     /* Use modern Thumb-2 syntax */
+.cpu cortex-m3
+.thumb
 
 .section .text
-    .align 1              // align next label (systick_Handler) to 2-byte boundary for correct instruction fetch
-    .type systick_Handler, %function   // tells the assembler to treat this as a function
+.p2align 1              // align next label (systick_Handler) to 2-byte boundary for correct instruction fetch
+.type systick_Handler, %function   // tells the assembler to treat this as a function
 
 .global systick_Handler
-
 systick_Handler:
 
 /* ---------------------------------------------------------
@@ -16,37 +17,25 @@ systick_Handler:
     stmdb sp!, {r4-r11}   // Save R4-R11 to the current stack (decrement SP)
 
 /* ---------------------------------------------------------
-        2. STORE CURRENT SP
-            /* task_sps[current_task] = SP
+       2. SAVE CURRENT SP to TCB
+       old_tcb->sp = SP
        --------------------------------------------------------- */
+    ldr r0, =current_tcb    // R0 = &current_tcb
+    ldr r1, [r0]            // R1 = current_tcb (Pointer to struct)
+    str sp, [r1]            // Store SP at [R1 + 0] (Offset 0 is SP)
 
-    ldr r0, =task_sps       /* R0 = Base addr of task_sps Array */
-    ldr r1, =current_task   /* R1 = Addr of current_task variable */
-    ldr r2, [r1]            /* R2 = Val. of current_task variable i.e., index */
-
-        /* Calculate addr of where current-SP must be stored in task_sps Array*/
-    lsl r3, r2, #2          /* R3 = index*4   */
-    str sp, [r0, r3]        /* Store current-SP into task_sps */
-
-
-/* ---------------------------------------------------------
-        3. CHOOSE NEXT TASK (Round-Robin)
-        Increment current_task to Choose next task
-        /* current_task = (current_task + 1 ) & 3
+    /* ---------------------------------------------------------
+       3. CHOOSE NEXT TASK (Linked List Traversal)
+       current_tcb = current_tcb->next
        --------------------------------------------------------- */
+    ldr r2, [r1, #4]        // R2 = Load [R1 + 4] (The 'next' pointer)
+    str r2, [r0]            // Update global 'current_tcb' to point to new task
 
-    add r2, r2, #1          /* Increment curr-index */
-    and r2, r2, #3          /* Wrap around */
-    str r2, [r1]            /* Update current_task variable in Memory */
-
-
-/* ---------------------------------------------------------
-        4. LOAD NEXT SP
-        /* SP = task_sps[current_task]
+    /* ---------------------------------------------------------
+       4. LOAD NEXT SP from TCB
+       SP = new_tcb->sp
        --------------------------------------------------------- */
-
-    lsl r3, r2, #2          /* R3 = new curr-index * 4 */
-    ldr sp, [r0, r3]        /* Load next task's SP into SP*/
+    ldr sp, [r2]            // Load SP from [R2 + 0]
 
 
 /* ---------------------------------------------------------
